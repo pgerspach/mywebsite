57f5c497acbd15deba79059c1e7c40337f66f85f
pgerspach <pgerspac@alumni.nd.edu>
2019-02-16 01:52:42 -0500

    just one project up, but it's not bad

---
+++ public/assets/js/main.js
@@ -0,0 +1,35 @@
+const headerAnimateKeypoint = 500;
+
+$(document).ready(() => {
+  let headerKeypoint =
+    $(window).scrollTop() >= headerAnimateKeypoint ? true : false;
+  if (headerKeypoint) {
+    $(".pageHeader").css("animation", "headerAnimateForwards 0s forwards");
+    $(".socialIcons").css("animation", "animateSocialForward 0s forwards");
+
+
+  } else {
+    $(".pageHeader").css("animation", "headerAnimateReverse 0s forwards");
+    $(".socialIcons").css("animation", "animateSocialReverse 0s forwards");
+
+  }
+  $(window).on("scroll", event => {
+    if (
+      $(window).scrollTop() >= headerAnimateKeypoint &&
+      headerKeypoint === false
+    ) {
+      $(".pageHeader").css("animation", "headerAnimateForwards .8s forwards");
+      $(".socialIcon").css("animation", "animateSocialForward 1s forwards");
+
+      headerKeypoint = true;
+    } else if (
+      $(window).scrollTop() < headerAnimateKeypoint &&
+      headerKeypoint === true
+    ) {
+      $(".pageHeader").css("animation", "headerAnimateReverse .8s forwards");
+      $(".socialIcon").css("animation", "animateSocialReverse 1s forwards");
+
+      headerKeypoint = false;
+    }
+  });
+});
